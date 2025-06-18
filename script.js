7d5a0899329763515307377671213897ac0fe9bf
B BHARATH KUMAR REDDY <bharathboienipalli@gmail.com>
2025-06-18 07:51:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
+const sampleObject = {
+  red: "#FF0000",
+  green: "#00FF00",
+  white: "#FFFFFF"
+};
 
 function hasKey(key) {
-  //   write your code here
+  return sampleObject.hasOwnProperty(key);
 }
 
-// Do not change the code below
-const key = prompt("Enter Key.");
-alert(hasKey(key));
+console.log(hasKey("red"));  
+console.log(hasKey("blue")); 
+console.log(hasKey("white")); 
